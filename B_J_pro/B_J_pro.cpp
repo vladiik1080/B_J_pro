8063425ea203714ba9c602e05770783b58ea3cc7
vladiik_1080 <vovaserk7@gmail.com>
2022-05-30 19:30:00 +0300

    Добавил свойства и методы в класс Card.

--- B_J_pro/B_J_pro.cpp
+++ B_J_pro/B_J_pro.cpp
@@ -1,17 +1,56 @@
 ﻿#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 class Card
 {
 public:
-
+	enum rank 
+	{
+		ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+	};
+	enum suit
+	{
+		CLUBS, DIAMONDS, HEARTS, SPADES
+	};
 
 private:
-
+	rank m_Rank;
+	suit m_Suit;
+	bool m_IsFaceUp;
+	Card(rank Rank, suit Suit, bool IsFaceUp);
+	int getValue() const;
+	void Flip();
 };
+
+Card::Card(rank Rank, suit Suit, bool IsFaceUp)
+{
+	m_Rank = Rank;
+	m_Suit = Suit;
+	m_IsFaceUp = IsFaceUp;
+}
+
+int Card::getValue() const
+{
+	int value = 0;
+	if (m_IsFaceUp)
+	{
+		value = m_Rank;
+		if (value > 10)
+		{
+			value = 10;
+		}
+	}
+	return value;
+}
+
+void Card::Flip()
+{
+	m_IsFaceUp = !(m_IsFaceUp);
+}
 
 class Hand
 {
