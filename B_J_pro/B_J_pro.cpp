71281e72ce771d2d6c13c4f529a435b8455430c7
vladiik_1080 <vovaserk7@gmail.com>
2022-05-30 16:31:12 +0300

    Добавьте файлы проекта.

---
+++ B_J_pro/B_J_pro.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Card
+{
+public:
+
+
+private:
+
+};
+
+class Hand
+{
+public:
+
+
+private:
+
+};
+
+class Deck :public Hand
+{
+public:
+
+
+private:
+
+};
+
+class GenericPlayer :public Hand
+{
+public:
+
+
+private:
+
+};
+
+class Player :public GenericPlayer
+{
+public:
+
+
+private:
+
+};
+
+class House :public GenericPlayer
+{
+public:
+
+
+private:
+
+};
+
+class Game
+{
+public:
+
+
+private:
+
+};
+
+int main()
+{
+	cout << "Test text, sorry)";
+}
+
+
+
