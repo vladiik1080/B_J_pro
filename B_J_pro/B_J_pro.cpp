30c371b8e3ee2569867826a356fcd64b1214900d
vladiik_1080 <vovaserk7@gmail.com>
2022-05-30 21:50:15 +0300

    Дописал.

--- B_J_pro/B_J_pro.cpp
+++ B_J_pro/B_J_pro.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 using namespace std;
-
+/////////////////////////////////////////////////////////////////////////////////////////
 class Card
 {
 public:
@@ -19,6 +19,7 @@ public:
 	Card(rank r = ACE, suit s = SPADES, bool ifu = true);
 	int getValue() const;
 	void Flip();
+	friend ostream& operator<<(ostream& os, const Card& aCard);
 private:
 	rank m_Rank;
 	suit m_Suit;
@@ -50,7 +51,7 @@ void Card::Flip()
 {
 	m_IsFaceUp = !(m_IsFaceUp);
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////
 class Hand
 {
 public:
@@ -60,7 +61,7 @@ public:
 	void Clear();
 	int getTotal() const;
 
-private:
+protected:
 	vector <Card*> m_Cards;
 };
 
@@ -118,54 +119,335 @@ int Hand::getTotal() const
 	}
 	return total;
 }
+/////////////////////////////////////////////////////////////////////////////////////////
+class GenericPlayer : public Hand
+{
+	friend ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer);
+public:
+	GenericPlayer(const string& name = "");
+	virtual ~GenericPlayer();
+	virtual bool IsHitting() const = 0;
+	bool IsBusted() const;
+	void Bust() const;
+protected:
+	string m_Name;
+};
 
+GenericPlayer::GenericPlayer(const string& name) : m_Name(name) {}
+
+GenericPlayer::~GenericPlayer() {}
+
+bool GenericPlayer::IsBusted() const
+{
+	return (getTotal() > 21);
+}
+
+void GenericPlayer::Bust() const
+{
+	cout << m_Name << " busts.\n";
+}
+/////////////////////////////////////////////////////////////////////////////////////////
 class Deck :public Hand
 {
 public:
-
+	Deck();
+	virtual ~Deck();
+	void Populate();
+	void Shuffle();
+	void Deal(Hand& aHand);
+	void AdditionalCards(GenericPlayer& aGenericPlayer);
 
 private:
 
 };
 
-class GenericPlayer :public Hand
+Deck::Deck()
 {
-public:
+	m_Cards.reserve(52);
+	Populate();
+}
 
+Deck::~Deck() {}
 
-private:
+void Deck::Populate()
+{
+	Clear();
+	for (int s = Card::CLUBS; s <= Card::SPADES; ++s)
+	{
+		for (int r = Card::ACE; r <= Card::KING; ++r)
+		{
+			Add(new Card(static_cast <Card::rank>(r), static_cast <Card::suit>(s)));
+		}
+	}
+}
 
-};
+void Deck::Shuffle()
+{
+	random_shuffle(m_Cards.begin(), m_Cards.end());
+}
 
+void Deck::Deal(Hand& aHand)
+{
+	if (!m_Cards.empty())
+	{
+		aHand.Add(m_Cards.back());
+		m_Cards.pop_back();
+	}
+	else
+	{
+		cout << "Out of cards, Unable to deal.\n";
+	}
+}
+
+void Deck::AdditionalCards(GenericPlayer& aGenericPlayer)
+{
+	cout << endl;
+	while (!(aGenericPlayer.IsBusted()) && aGenericPlayer.IsHitting())
+	{
+		Deal(aGenericPlayer);
+		cout << aGenericPlayer << endl;
+
+		if (aGenericPlayer.IsBusted())
+			aGenericPlayer.Bust();
+	}
+}
+/////////////////////////////////////////////////////////////////////////////////////////
 class Player :public GenericPlayer
 {
 public:
-
+	Player(const string& name = "");
+	virtual ~Player();
+	virtual bool IsHitting() const;
+	void Win() const;
+	void Lose() const;
+	void Push() const;
 
 private:
 
 };
 
+Player::Player(const string& name) : GenericPlayer(name) {}
+
+Player::~Player() {}
+
+bool Player::IsHitting() const
+{
+	cout << m_Name << ", do you want a hit? (Y/N): ";
+	char response;
+	cin >> response;
+	return (response == 'y' || response == 'Y');
+}
+
+void Player::Win() const
+{
+	cout << m_Name << " wins.\n";
+}
+
+void Player::Lose() const
+{
+	cout << m_Name << " loses.\n";
+}
+
+void Player::Push() const
+{
+	cout << m_Name << " pushes.\n";
+}
+/////////////////////////////////////////////////////////////////////////////////////////
 class House :public GenericPlayer
 {
 public:
-
+	House(const string& name = "House");
+	virtual ~House();
+	virtual bool IsHitting() const;
+	void FlipFirstCard();
 
 private:
 
 };
 
+House::House(const string& name) : GenericPlayer(name) {}
+
+House::~House() {}
+
+bool House::IsHitting() const
+{
+	return (getTotal() <= 16);
+}
+
+void House::FlipFirstCard()
+{
+	if (!(m_Cards.empty()))
+		m_Cards[0]->Flip();
+
+	else
+		cout << "No card to flip!\n";
+}
+/////////////////////////////////////////////////////////////////////////////////////////
 class Game
 {
 public:
-
+	Game(const vector<string>& names);
+	~Game();
+	void Play();
+	friend void Info();
 
 private:
-
+	Deck m_Deck;
+	House m_House;
+	vector<Player> m_Players;
 };
+
+Game::Game(const vector<string>& names)
+{
+	vector<string>::const_iterator pName;
+	for (pName = names.begin(); pName != names.end(); ++pName)
+	{
+		m_Players.push_back(Player(*pName));
+	}
+	srand(time(0));
+	m_Deck.Populate();
+	m_Deck.Shuffle();
+}
+
+Game::~Game() {}
+
+void Game::Play()
+{
+	vector<Player>::iterator pPlayer;
+	for (int i = 0; i < 2; ++i)
+	{
+		for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+			m_Deck.Deal(*pPlayer);
+
+		m_Deck.Deal(m_House);
+	}
+	m_House.FlipFirstCard();
+
+	for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+		cout << *pPlayer << endl;
+
+	cout << m_House << endl;
+
+	for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+		m_Deck.AdditionalCards(*pPlayer);
+
+	m_House.FlipFirstCard();
+	cout << endl << m_House;
+
+	m_Deck.AdditionalCards(m_House);
+
+	if (m_House.IsBusted())
+	{
+		for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+			if (!(pPlayer->IsBusted()))
+				pPlayer->Win();
+	}
+	else
+	{
+		for (pPlayer = m_Players.begin(); pPlayer != m_Players.end();
+			++pPlayer)
+			if (!(pPlayer->IsBusted()))
+			{
+				if (pPlayer->getTotal() > m_House.getTotal())
+					pPlayer->Win();
+
+				else if (pPlayer->getTotal() < m_House.getTotal())
+					pPlayer->Lose();
+
+				else
+					pPlayer->Push();
+			}
+	}
+	for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+		pPlayer->Clear();
+
+	m_House.Clear();
+}
+/////////////////////////////////////////////////////////////////////////////////////////
+ostream& operator<<(ostream& os, const Card& aCard)
+{
+	const string RANKS[] = { "0", "A", "2", "3", "4", "5", "6", "7", "8", "9",
+							 "10", "J", "Q", "K" };
+
+	const string SUITS[] = { "c", "d", "h", "s" };
+
+	if (aCard.m_IsFaceUp)
+		os << RANKS[aCard.m_Rank] << SUITS[aCard.m_Suit];
+	else
+		os << "XX";
+
+	return os;
+}
+
+ostream& operator <<(ostream& os, const GenericPlayer& aGenericPlayer)
+{
+	os << aGenericPlayer.m_Name << ":\t";
+
+	vector <Card*>::const_iterator pCard;
+	if (!aGenericPlayer.m_Cards.empty())
+	{
+		for (pCard = aGenericPlayer.m_Cards.begin();
+			pCard != aGenericPlayer.m_Cards.end(); ++pCard)
+			os << *(*pCard) << "\t";
+
+		if (aGenericPlayer.getTotal() != 0)
+			cout << "(" << aGenericPlayer.getTotal() << ")";
+	}
+	else
+		os << "<empty>";
+
+	return os;
+}
+/////////////////////////////////////////////////////////////////////////////////////////
+
+void Info()
+{
+	cout << "\tGoal: Reach 21 without going over\n\n";
+	cout << "\tThe House (computer player) will be playing against you\n\n";
+	cout << "\tIf the House busts (goes over), all players who\n";
+	cout << "\thaven't busted will win.\n\n";
+	cout << "\tIf the House doesn't bust, all players\n";
+	cout << "\twho haven't busted, will win, if their\n";
+	cout << "\ttotal value is greater than the House's\n\n";
+	cout << "\tIf a player gets 21 and the House doesn't\n";
+	cout << "\tthe player is a winner\n\n";
+	cout << "\tIf a player and the House both get 21\n";
+	cout << "\tit is declared a push (tie)\n\n";
+	cout << "\tUp to seven players may play\n\n";
+	cout << "\tc = clubs, d = diamonds, h = hearts, s = spades\n\n";
+}
 
 int main()
 {
+	cout << "\t\tWelcome to Blackjack! Have fun playing!\n\n";
+	Info();
+
+	int numPlayers = 0;
+	while (numPlayers < 1 || numPlayers > 7)
+	{
+		cout << "How many players? (1 - 7): ";
+		cin >> numPlayers;
+	}
+	vector <string> names;
+	string name;
+	for (int i = 0; i < numPlayers; ++i)
+	{
+		cout << "Enter player name: ";
+		cin >> name;
+		names.push_back(name);
+	}
+
+	cout << endl;
+
+	Game aGame(names);
+	char again = 'y';
+	while (again != 'n' && again != 'N')
+	{
+		aGame.Play();
+		cout << "\nDo you want to play again? (Y/N): ";
+		cin >> again;
+	}
+
 	return 0;
 }
 
